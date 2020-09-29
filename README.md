d673c437db5a8927acdf0ce7e2584ebc7e575622
kasthurimahadeva <kasthuri.14@cse.mrt.ac.lk>
2020-09-29 15:59:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-editor-wysiwyg-ceg4hp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-editor-wysiwyg-ceg4hp)
---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular 6';
+  htmlContent = '';
+
+  config: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '15rem',
+    minHeight: '5rem',
+    placeholder: 'Enter text here...',
+    translate: 'no',
+    defaultParagraphSeparator: 'p',
+    defaultFontName: 'Arial',
+    customClasses: [
+      {
+        name: "quote",
+        class: "quote",
+      },
+      {
+        name: 'redText',
+        class: 'redText'
+      },
+      {
+        name: "titleText",
+        class: "titleText",
+        tag: "h1",
+      },
+    ]
+  };
+}
